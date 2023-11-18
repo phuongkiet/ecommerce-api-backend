13522913e3dc6b8ab1bfa9df085d53740dc2f811
phuongkiet <kietnp15092003@gmail.com>
2023-11-19 03:12:02 +0700

    Add axios and react-router-dom for Routing config

--- src/main/resources/static/ecommerce-frontend/src/App.js
+++ src/main/resources/static/ecommerce-frontend/src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ListProductComponent from "./components/ListProductComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
-  return (
-    <div className="container">
-        <ListProductComponent/>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <div className={"container"}>
+                    <HeaderComponent/>
+                    <div className={"container"}>
+                        <Routes>
+                            <Route path = "/" element = {<ListProductComponent />}></Route>
+                            <Route path = "/product/getAll" element = {<ListProductComponent />}></Route>
+                        </Routes>
+                    </div>
+                    <FooterComponent/>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
