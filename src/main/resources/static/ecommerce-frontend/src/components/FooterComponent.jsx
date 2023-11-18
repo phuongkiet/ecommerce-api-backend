fe696f26460972a4ec5e70bd787dd05e2193caa5
phuongkiet <kietnp15092003@gmail.com>
2023-11-19 03:12:21 +0700

    Add header and footer for web

---
+++ src/main/resources/static/ecommerce-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class FooterComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+                <footer className={"footer"}>
+                    <span>All rights reserved 2023 @phuongkiet</span>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default FooterComponent;
